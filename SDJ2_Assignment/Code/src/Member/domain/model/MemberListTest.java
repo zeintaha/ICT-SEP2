2dd0df3f0b108eec58adae9f7e1da07270dc2cb7
mouhanadeem <31960661+mouhanadeem@users.noreply.github.com>
2018-05-03 19:44:10 +0200

    list test

--- SDJ2_Assignment/Code/src/Member/domain/model/MemberListTest.java
+++ SDJ2_Assignment/Code/src/Member/domain/model/MemberListTest.java
@@ -9,16 +9,18 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MemberListTest {
-private MemberList list= new MemberList();
 	Member member1 = new Member("Taha", 26, "paid");
 	Member member2 = new Member("Alex", 24, "not paid");
 	Member member3 = new Member("Oskar", 22, "not paid");
+	MemberList list = new MemberList();
+
 	@Test
 	public void testAddMember() {
 		list.addMember(member1);
 		list.addMember(member2);
 		list.addMember(member3);
 	}
+
 	@Test
 	public void testGetsNumberOfMembers() {
 		list.addMember(member1);
@@ -26,6 +28,7 @@ private MemberList list= new MemberList();
 		list.addMember(member3);
 		assertEquals(3, list.getNumberOfMembers());
 	}
+
 	@Test
 	public void testGetsMember() {
 		list.addMember(member1);
@@ -35,6 +38,7 @@ private MemberList list= new MemberList();
 		assertEquals(member2, list.getMember(1));
 		assertEquals(member3, list.getMember(2));
 	}
+
 	@Test
 	public void testGetsMembershipNotPaid() {
 		list.addMember(member1);
