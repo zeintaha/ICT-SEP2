da39b29e9210227d584ae437967b8261c58f65c2
fadi <fadi@githup.com>
2018-05-18 11:35:23 +0200

    sd

--- code/src/StaffMediator/StaffPersistance.java
+++ code/src/StaffMediator/StaffPersistance.java
@@ -1,6 +1,12 @@
 package StaffMediator;
 
+import java.io.IOException;
+
+import Staff.Employee;
+
+
+
 public interface StaffPersistance
 {
-
+   public void save(Employee employee) throws IOException;
 }
