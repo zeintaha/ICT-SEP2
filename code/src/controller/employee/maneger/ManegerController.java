ae74ce660864c54c6a1984f4bbe9f18b928a8359
fadi <fadi@githup.com>
2018-05-22 10:45:29 +0200

    hi

---
+++ code/src/controller/employee/maneger/ManegerController.java
@@ -0,0 +1,29 @@
+package controller.employee.maneger;
+import javax.swing.JButton;
+
+
+
+import com.sun.glass.ui.View;
+
+import domain.Facad;
+import domain.mediator.staff.StaffClinicModel;
+
+public class ManegerController
+{
+   private View addManeger;
+   private Facad facad;
+   
+   public ManegerController(Facad facad, View addManeger) {
+      this.facad=facad;
+      this.addManeger=addManeger;
+      
+     
+
+   }
+   
+   public void executes(String what) {
+      
+   }
+   
+
+}
