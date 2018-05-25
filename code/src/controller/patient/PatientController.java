1b2d8edd3209a77b4682fc26bc966e311f99ee9f
fadi <fadi@githup.com>
2018-05-25 10:53:25 +0200

    new

--- code/src/controller/patient/PatientController.java
+++ code/src/controller/patient/PatientController.java
@@ -1,18 +1,15 @@
 package controller.patient;
 import java.io.IOException;
 
-import view.patient.searchpatient.*;
-import domain.Facad;
 import domain.mediator.patient.PatientModel;
-import domain.mediator.patient.PatientModelManager;
 import domain.model.patient.Patient;
-import domain.model.patient.PatientList;
+import view.patient.searchpatient.SearchPatientGUI;
 public class PatientController {
 
 	   private PatientModel patientModel;
 	  
 	   private SearchPatientGUI addpatient;
-	   private Facad facad;
+	  
 	  
 	   
 	   public PatientController(PatientModel patientModel, SearchPatientGUI view) throws ClassNotFoundException, IOException {
